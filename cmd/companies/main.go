652e6cf077627a63202bddb30bf9a1b454a1cd18
SIIIKOR <siiikor.sikorski@gmail.com>
2023-05-15 17:56:58 +0200

    test

---
+++ cmd/companies/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/msik-404/micro-appoint-companies/internal/database"
+	"net/http"
+)
+
+func main() {
+    db := database.ConnectDB()
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
---
+++ internal/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"github.com/msik-404/micro-appoint-companies/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=psql user=%s password=%s dbname=%s port=5432 TimeZone=CET",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database.\n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Company{}, &models.Service{})
+
+    return db
+}
