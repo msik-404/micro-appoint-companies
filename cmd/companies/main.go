fc48e6bb0eebf19a460e8691aa3dfe954ce3182e
SIIIKOR <siiikor.sikorski@gmail.com>
2023-05-16 21:38:15 +0200

    connecting to mongodb works

--- cmd/companies/main.go
+++ cmd/companies/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/msik-404/micro-appoint-companies/internal/database"
 	"net/http"
 )
 
 func main() {
+	database.ConnectDB()
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
-	})
+		})
 	})
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ internal/database/database.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func getURI() string {
+	return fmt.Sprintf("mongodb://%s:%s@mongodb:27017", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+}
+
+func ConnectDB() *mongo.Client {
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(getURI()).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+    return client
+}
