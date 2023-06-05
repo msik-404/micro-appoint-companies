b99a9073f6d92055ce8f8bb4bc171260d88e2b7c
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-06 00:00:46 +0200

    removed rest

--- cmd/companies/main.go
+++ cmd/companies/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	// "github.com/gin-gonic/gin"
-
 	"fmt"
 	"net"
 
 	"github.com/msik-404/micro-appoint-companies/internal/communication"
 	"github.com/msik-404/micro-appoint-companies/internal/database"
 	"google.golang.org/grpc"
-	// "github.com/msik-404/micro-appoint-companies/internal/controllers/companies"
-	// "github.com/msik-404/micro-appoint-companies/internal/controllers/services"
 )
 
 func main() {
@@ -32,22 +28,4 @@ func main() {
     if err := s.Serve(lis); err != nil {
         panic(err)
     }
-
-	// r := gin.Default()
-
-	// r.GET("/companies", companies.GetCompaniesEndPoint(db))
-
-	// r.GET("/companies/:id", companies.GetCompanyEndPoint(db))
-	// r.GET("/companies/services/:id", services.GetServicesEndPoint(db))
-
-	// r.POST("/companies", companies.AddCompanyEndPoint(db))
-    // r.POST("/companies/services/:id", services.AddServiceEndPoint(db))
-
-	// r.PUT("/companies/:id", companies.UpdateCompanyEndPoint(db))
-	// r.PUT("/services/:id", services.UpdateServiceEndPoint(db))
-
-	// r.DELETE("/companies/:id", companies.DeleteCompanyEndPoint(db))
-	// r.DELETE("/services/:id", services.DeleteServiceEndPoint(db))
-
-	// r.Run() // listen and serve on 0.0.0.0:8080
 }
